d8aae16f4ae69fc0fc375da77a974c49e003bac2
ferz <s5005a@gmail.com>
2011-10-21 15:53:53 +0300

    просмотри изображений, зум, подгонка под размер окна

--- mainwindow.h
+++ mainwindow.h
@@ -2,8 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QtSql>
-
+#include "qscrollbar.h"
+#include "qdir.h"
 
 namespace Ui {
     class MainWindow;
@@ -18,7 +18,30 @@ public:
     ~MainWindow();
 
 private:
+    double scaleFactor;
     Ui::MainWindow *ui;
+    void updateActions();
+    void adjustScrollBar(QScrollBar *scrollBar, double factor);
+    void scaleImage(double factor);
+    void openImg(QString fileName);
+    QStringList fnlist;
+    QStringList namefilter;
+    QDir *pdr;
+    int ci;
+
+private slots:
+
+    void openfile();
+    void aboutProg();
+    void help();
+    void exitApp();
+    void zoomIn();
+    void zoomOut();
+    void fitToWindow();
+    void normalSize();
+    void next();
+    void prev();
+
 };
 
 #endif // MAINWINDOW_H
